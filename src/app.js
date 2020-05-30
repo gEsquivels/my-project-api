9c8ed250955e1b7cf90e484eda9cd57a7c5e0582
Guilherme Esquivel <guilhermesantos1507@gmail.com>
2020-05-30 17:08:42 -0300

    Corrigindo erro de produção

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(accessAuthorization);
 app.use(helmet());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(routes);
 
