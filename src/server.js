d31e6b387c94c48a7164445aeb5b6cee883257ec
Guilherme Esquivel <guilhermesantos1507@gmail.com>
2020-05-30 15:48:27 -0300

    Mais configurações de deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const app = require('./app.js');
-const config = require('./config/server');
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
   if (err) throw err
   console.info(`Server is runnig`);
 });
