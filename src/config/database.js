bdaf009271962c6b1378c1e38d6b352fb68c2c10
Guilherme Esquivel <guilhermesantos1507@gmail.com>
2020-05-30 15:21:17 -0300

    Configurando banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'postegres',
-    database: 'project',
+    host: 'ec2-54-175-117-212.compute-1.amazonaws.com',
+    username: 'mvrsobbbkznqbu',
+    password: 'b96087a3fd27a65d771ceb407068edc9400c7ede9badf52de50bac1b08cdbbe9',
+    database: 'd3lpblg5keuuj2',
     define: {
         timestamps: true,
         underscored: true,
